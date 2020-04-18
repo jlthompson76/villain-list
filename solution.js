5ad97a49ca599400f4e091edd1a60f0395627378
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-18 11:30:29 -0400

    Complete 1.1 thru 1.5, still need to finish 1.6 thru 1.8

--- solution.js
+++ solution.js
@@ -1,27 +1,78 @@
 ////////////// Part 1 /////////////////////
 /* 1.1) Set the classes of the inputs (buttons) to "btn" by using a loop */
 
+let inputButtons = document.querySelectorAll('input[type = button]');
+console.log(inputButtons);
+for (let i = 0; i < inputButtons.length; i++) {
+        inputButtons[i].className = "btn";
+}
 
-/*  1.2) Set the classes of the inputs (text) to txtBox by using a loop */
+/*  1.2) Set the classes of the inputs (text) to txt-box by using a loop */
 
-
+let inputText = document.querySelectorAll('input[type = text');
+console.log(inputText);
+for (let i = 0; i < inputText.length; i++) {
+        inputText[i].className = "txt-box";
+}
 
 /* 1.3) grab both <ul> that we are going to append <li> nodes to and store in variables */
 
+let wantedList = document.querySelector("#wanted-list");
+console.log(wantedList);
+
+let caughtList = document.querySelector("#caught-list");
+console.log(caughtList);
 
 /* 1.4) Create a function called addVillain that gets the value of the textbox and adds it to the UL.
         You also want to update the wanted count next to the h2 tag.
         Tip: you can also set the value to the text input to an empty string when you're done.
 */
 
+document.querySelector('#btn-add').addEventListener('click', addVillain);
 
+function addVillain() {
+        let villain = document.querySelector('#txt-villain').value;
+        let li = document.createElement("li");
+        li.innerText = villain;
+        wantedList.appendChild(li);
+        let wantedCount = wantedList.querySelectorAll('li').length;
+        document.querySelector("#wanted-count").innerText = wantedCount;
+        document.querySelector('#txt-villain').value = "";
+}
 
 /*  1.5) Create a function called caughtVillain that gets the value of the textbox and adds it to the UL.
          You also want to remove the caught villain from the wanted list.
          You also want to update the wanted and caught counts next to the h2 tags.
      */
 
+document.querySelector('#btn-caught').addEventListener('click', caughtVillain);
 
+function caughtVillain() {
+        let villain = document.querySelector('#txt-villain').value;
+        let li = document.createElement("li");
+        li.innerText = villain;
+        caughtList.appendChild(li);
+
+        // Remove Caught Villain from Wanted List
+        wantedVillains = wantedList.querySelectorAll('li');
+        for (let i = 0; i < wantedVillains.length; i++) {
+                if (wantedVillains[i].innerText === villain) {
+                        wantedVillains[i]
+                        .remove();
+                }
+        }
+        
+        // Update Caught Count
+        let caughtCount = caughtList.querySelectorAll('li').length;
+        document.querySelector("#caught-count").innerText = caughtCount;
+
+        // Update Wanted Caught
+        let wantedCount = wantedList.querySelectorAll('li').length;
+        document.querySelector("#wanted-count").innerText = wantedCount;
+
+        // Reset Text Box
+        document.querySelector('#txt-villain').value = "";
+}
 
 /* 1.6) Create a function called getTotal that displays the total number of villains that are on both lists.
         You should display it in the #item-count
@@ -36,4 +87,4 @@
 
 
 
-/* 1.8) Bind all the clicks to the the appropriate inputs and functions by using event listeners  */
+/* 1.8) Bind all the clicks to the appropriate inputs and functions by using event listeners  */
