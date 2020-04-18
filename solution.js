77026c3d582cd7de2a6cb9b9f7a7c58768aaefe0
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-18 09:42:15 -0400

    Initial commit

---
+++ solution.js
@@ -0,0 +1,39 @@
+////////////// Part 1 /////////////////////
+/* 1.1) Set the classes of the inputs (buttons) to "btn" by using a loop */
+
+
+/*  1.2) Set the classes of the inputs (text) to txtBox by using a loop */
+
+
+
+/* 1.3) grab both <ul> that we are going to append <li> nodes to and store in variables */
+
+
+/* 1.4) Create a function called addVillain that gets the value of the textbox and adds it to the UL.
+        You also want to update the wanted count next to the h2 tag.
+        Tip: you can also set the value to the text input to an empty string when you're done.
+*/
+
+
+
+/*  1.5) Create a function called caughtVillain that gets the value of the textbox and adds it to the UL.
+         You also want to remove the caught villain from the wanted list.
+         You also want to update the wanted and caught counts next to the h2 tags.
+     */
+
+
+
+/* 1.6) Create a function called getTotal that displays the total number of villains that are on both lists.
+        You should display it in the #item-count
+        Tip: You need to set the className of the total-area to just "total-area" for it to show up.
+*/
+
+
+/* 1.7) Create a function called getStatus that searches all li elements for the villain the user is looking for.
+        You will need to find the parent element of the li once there is a match.
+        Whichever list the villain is on, create an alert message.
+*/
+
+
+
+/* 1.8) Bind all the clicks to the the appropriate inputs and functions by using event listeners  */
