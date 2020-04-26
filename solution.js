4a810aa05a9158aa405fe82f1fbb24a5be535b7f
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-26 18:08:36 -0400

    Finish JavaScript file

--- solution.js
+++ solution.js
@@ -1,7 +1,14 @@
+// Declare Variables
+let inputButtons, inputText, wantedList, caughtList, villain, wantedCount, caughtCount, li, wantedVillains, total, totalArea, allVillains, villainList, villainStatus;
+
+// Set Initial Counts to Zero
+wantedCount = 0;
+caughtCount = 0;
+
 ////////////// Part 1 /////////////////////
 /* 1.1) Set the classes of the inputs (buttons) to "btn" by using a loop */
 
-let inputButtons = document.querySelectorAll('input[type = button]');
+inputButtons = document.querySelectorAll('input[type = button]');
 console.log(inputButtons);
 for (let i = 0; i < inputButtons.length; i++) {
         inputButtons[i].className = "btn";
@@ -9,18 +16,17 @@ for (let i = 0; i < inputButtons.length; i++) {
 
 /*  1.2) Set the classes of the inputs (text) to txt-box by using a loop */
 
-let inputText = document.querySelectorAll('input[type = text');
-console.log(inputText);
+inputText = document.querySelectorAll('input[type = text');
 for (let i = 0; i < inputText.length; i++) {
         inputText[i].className = "txt-box";
 }
 
 /* 1.3) grab both <ul> that we are going to append <li> nodes to and store in variables */
 
-let wantedList = document.querySelector("#wanted-list");
+wantedList = document.querySelector("#wanted-list");
 console.log(wantedList);
 
-let caughtList = document.querySelector("#caught-list");
+caughtList = document.querySelector("#caught-list");
 console.log(caughtList);
 
 /* 1.4) Create a function called addVillain that gets the value of the textbox and adds it to the UL.
@@ -28,16 +34,40 @@ console.log(caughtList);
         Tip: you can also set the value to the text input to an empty string when you're done.
 */
 
-document.querySelector('#btn-add').addEventListener('click', addVillain);
+// Refactored code to create additional functions, in addition to addVillain function below
+
+function getVillain() {
+        villain = document.querySelector('#txt-villain').value;
+}
+
+function resetTextBox() {
+        document.querySelector('#txt-villain').value = "";
+}
+
+function countWanted() {
+        wantedCount = wantedList.querySelectorAll('li').length;
+        document.querySelector("#wanted-count").innerText = wantedCount;
+}
+
+function countCaught() {
+        caughtCount = caughtList.querySelectorAll('li').length;
+        document.querySelector("#caught-count").innerText = caughtCount;
+}
 
 function addVillain() {
-        let villain = document.querySelector('#txt-villain').value;
+        // Capture Textbox Value
+        getVillain();
+
+        // Add Villain to Wanted List
         let li = document.createElement("li");
         li.innerText = villain;
         wantedList.appendChild(li);
-        let wantedCount = wantedList.querySelectorAll('li').length;
-        document.querySelector("#wanted-count").innerText = wantedCount;
-        document.querySelector('#txt-villain').value = "";
+
+        // Update Wanted Count
+        countWanted();
+
+        // Reset Text Box
+        resetTextBox();
 }
 
 /*  1.5) Create a function called caughtVillain that gets the value of the textbox and adds it to the UL.
@@ -45,16 +75,18 @@ function addVillain() {
          You also want to update the wanted and caught counts next to the h2 tags.
      */
 
-document.querySelector('#btn-caught').addEventListener('click', caughtVillain);
-
 function caughtVillain() {
-        let villain = document.querySelector('#txt-villain').value;
-        let li = document.createElement("li");
+        // Capture Textbox Value
+        getVillain();
+
+        // Add Villain to Caught List
+        li = document.createElement("li");
         li.innerText = villain;
         caughtList.appendChild(li);
 
         // Remove Caught Villain from Wanted List
         wantedVillains = wantedList.querySelectorAll('li');
+        // let wantedVillains = wantedList.childNodes; // alternate code shown by Sonia in class
         for (let i = 0; i < wantedVillains.length; i++) {
                 if (wantedVillains[i].innerText === villain) {
                         wantedVillains[i]
@@ -63,15 +95,13 @@ function caughtVillain() {
         }
         
         // Update Caught Count
-        let caughtCount = caughtList.querySelectorAll('li').length;
-        document.querySelector("#caught-count").innerText = caughtCount;
+        countCaught();
 
         // Update Wanted Caught
-        let wantedCount = wantedList.querySelectorAll('li').length;
-        document.querySelector("#wanted-count").innerText = wantedCount;
+        countWanted();
 
         // Reset Text Box
-        document.querySelector('#txt-villain').value = "";
+        resetTextBox();
 }
 
 /* 1.6) Create a function called getTotal that displays the total number of villains that are on both lists.
@@ -79,12 +109,65 @@ function caughtVillain() {
         Tip: You need to set the className of the total-area to just "total-area" for it to show up.
 */
 
+function getTotal() {
+        // Calculate Total from Wanted & Caught Lists
+        total = wantedCount + caughtCount;
+
+        // Display Total
+        document.querySelector('#item-count').textContent = `Total Villains: ${total}`;
+        totalArea = document.querySelector('.total-area');
+        totalArea.className = "total-area";
+}
 
 /* 1.7) Create a function called getStatus that searches all li elements for the villain the user is looking for.
         You will need to find the parent element of the li once there is a match.
         Whichever list the villain is on, create an alert message.
 */
 
+function getStatus() {
+        // Capture Textbox Value
+        getVillain();
 
+        // Select All Villains
+        allVillains = document.querySelectorAll('li');
+        console.log(allVillains);
+
+        // Search All Villains for a Match
+        for (i = 0; i < allVillains.length; i++) {
+
+                // If match found, determine which list the villain is on
+                console.log(allVillains[i].textContent);
+                if (allVillains[i].textContent === villain) {
+                        villainList = allVillains[i].parentElement.id;
+                        console.log(villainList);
+                        if (villainList === "wanted-list") {
+                                villainList = "Wanted List";
+                        } else if (villainList === "caught-list") {
+                                villainList = "Caught List";
+                        }
+                        villainStatus = `${villain} is on the ${villainList}.`;
+                        break;
+
+                // If match not found, villain isn't on either list
+                } else {
+                        villainStatus = `${villain} isn't currently on the Wanted or Caught list.`;
+                }
+        }
+
+        // Display Alert Message to User
+        alert(villainStatus);
+}
 
 /* 1.8) Bind all the clicks to the appropriate inputs and functions by using event listeners  */
+
+// Add event listener to ADD villain button
+document.querySelector('#btn-add').addEventListener('click', addVillain);
+
+// Add event listener to CAUGHT villain button
+document.querySelector('#btn-caught').addEventListener('click', caughtVillain);
+
+// Add event listener to GET TOTAL button
+document.querySelector('#btn-total').addEventListener('click', getTotal);
+
+// Add event listener to GET status button
+document.querySelector('#btn-status').addEventListener('click', getStatus);
